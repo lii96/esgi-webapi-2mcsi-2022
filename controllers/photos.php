f52d0506219ab1faff755e4f5eaa8f72327073d9
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-03-31 11:11:25 +0200

    added more controllers

---
+++ controllers/photos.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "TODO: controllers photos";
--- index.php
+++ index.php
@@ -1,21 +1,96 @@
 <?php
 
+/**
+ * Route correspondant à un fichier ou dossier qui n'existe pas
+ * @see https://www.php.net/manual/en/reserved.variables.request.php
+ */
 $route = $_REQUEST["route"];
 
+// Si la route est "users"
 if ($route === "users") {
+    /**
+     * Importe le contenu du fichier "controllers/users.php"
+     * @see https://www.php.net/manual/en/function.include.php
+     */
     include __DIR__ . "/controllers/users.php";
+    // Arrête le script à cet endroit
     die();
 }
 
+// Si la route est "comments"
 if ($route === "comments") {
+    /**
+     * Importe le contenu du fichier "controllers/comments.php"
+     * @see https://www.php.net/manual/en/function.include.php
+     */
     include __DIR__ . "/controllers/comments.php";
+
+    /**
+     * Arrête l'exécution du script
+     * @see https://www.php.net/manual/en/function.die.php
+     */
     die();
 }
 
-// Controlleur pour les albums
-// Controlleur pour les todos
-// Controlleur pour posts
-// Controlleur pour photos
+// Si la route est "albums"
+if ($route === "albums") {
+    /**
+     * Importe le contenu du fichier "controllers/albums.php"
+     * @see https://www.php.net/manual/en/function.include.php
+     */
+    include __DIR__ . "/controllers/albums.php";
 
-echo "Ce code ne s'affiche pas";
-echo "Après ce code";
+    /**
+     * Arrête l'exécution du script
+     * @see https://www.php.net/manual/en/function.die.php
+     */
+    die();
+}
+
+// Si la route est "todos"
+if ($route === "todos") {
+    /**
+     * Importe le contenu du fichier "controllers/todos.php"
+     * @see https://www.php.net/manual/en/function.include.php
+     */
+    include __DIR__ . "/controllers/todos.php";
+
+    /**
+     * Arrête l'exécution du script
+     * @see https://www.php.net/manual/en/function.die.php
+     */
+    die();
+}
+
+// Si la route est "posts"
+if ($route === "posts") {
+    /**
+     * Importe le contenu du fichier "controllers/posts.php"
+     * @see https://www.php.net/manual/en/function.include.php
+     */
+    include __DIR__ . "/controllers/posts.php";
+
+    /**
+     * Arrête l'exécution du script
+     * @see https://www.php.net/manual/en/function.die.php
+     */
+    die();
+}
+
+// Si la route est "photos"
+if ($route === "photos") {
+    /**
+     * Importe le contenu du fichier "controllers/photos.php"
+     * @see https://www.php.net/manual/en/function.include.php
+     */
+    include __DIR__ . "/controllers/photos.php";
+
+    /**
+     * Arrête l'exécution du script
+     * @see https://www.php.net/manual/en/function.die.php
+     */
+    die();
+}
+
+// Si aucun fichier ou dossier ne nous convient
+echo "Not found";
