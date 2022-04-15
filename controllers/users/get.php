d700d1502150e234a67206ace49472f103d19fb7
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-04-15 15:04:11 +0200

    added database connection

--- controllers/users/get.php
+++ controllers/users/get.php
@@ -1,24 +1,14 @@
 <?php
 
 include __DIR__ . "/../../functions/json-response.php";
-
-$driver = "mysql";
-
-$databaseName = "esgi-webapi-2mcsi-2022";
-
-$hostName = "localhost";
-
-$dataSourceName = "$driver:dbname=$databaseName;host=$hostName";
-
-$username = "root";
-
-$password = "root";
-
-$options = [];
+include __DIR__ . "/../../functions/get-database-connection.php";
 
 try {
-    $connection = new PDO($dataSourceName, $username, $password, $options);
-    jsonResponse(200, [], ["success" => true, "users" => []]);
+    $connection = getDatabaseConnection();
+    $query = $connection->query("SELECT * FROM users;");
+    $users = $query->fetchAll();
+
+    jsonResponse(200, [], ["success" => true, "users" => $users]);
 } catch (PDOException $exception) {
     $errorMessage = $exception->getMessage();
     jsonResponse(500, [], ["success" => false, "error" => "Error while trying to access the dabtabase: $errorMessage"]);
